7bf99e2827de961d9ef0432dc71c3b346c149741
“Konstantinos <kostaks@outlook.com>
2023-02-21 18:31:16 +0100

    done without styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react"
 import './App.css'
+import contacts from './contacts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstSeven, setFirstSeven] = useState(contacts.slice(0, 7))
+  const [remainingContacts, setRemainingContacts] = useState(contacts.slice(7))
+ 
+
+
+ //const random = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+  //console.log(random)
+
+ function addRandomContact() {
+  if (remainingContacts.length > 0) {
+  const random = Math.floor(Math.random() * remainingContacts.length);
+  const pickedContact = remainingContacts.splice(random, 1)[0];
+  setFirstSeven([...firstSeven, pickedContact])
+  console.log(remainingContacts.length)
+  }else{
+    alert("No more contacts")
+  }
+ }
+
+ function sortContactsByName() {
+  setFirstSeven([...firstSeven.sort((a, b) => a.name.localeCompare(b.name))])
+ }
+
+ function sortContactsByPopularity() {
+  setFirstSeven([...firstSeven.sort((a, b) => b.popularity - a.popularity)])
+ }
+
+ function deleteContact(id) {
+  const contactsRemaining = firstSeven.filter(contact => contact.id!== id)
+  setFirstSeven(contactsRemaining)
+  console.log(firstSeven)
+ }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  <div className='App'>
+    <h1>Ironcontacts</h1>
+    <button onClick={addRandomContact}>Press to add contact</button>
+    <button onClick={sortContactsByName}>Sort by Name</button>
+    <button onClick={sortContactsByPopularity}>Sort by Popularity</button>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+          {firstSeven.map((contact) => (
+            <tr key={contact.id} >
+              <td> <img src={contact.pictureUrl} className="image" alt="profile"></img></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              {contact.wonEmmy ? <td><img src='/public/trophy.png' ></img></td> : <td></td>}
+              {contact.wonOscar ? <td><img src='/public/trophy.png' ></img></td> : <td></td>}
+              <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
+        </tr>
+          ))}
+          
+       </tbody>
+    </table>
+
+  </div>
   )
 }
-
 export default App
